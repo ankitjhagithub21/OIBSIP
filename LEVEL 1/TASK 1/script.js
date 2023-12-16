5f615631b687723f456b6ccd0f283c63e15e3fe2
ankitjhagithub21 <ajha45075@gmail.com>
2023-12-16 08:29:18 +0530

    completed task 1

--- LEVEL 1/TASK 1/script.js
+++ LEVEL 1/TASK 1/script.js
@@ -1,28 +1,35 @@
 const menuBtn = document.getElementById('menuBtn')
 const menu = document.getElementById('menu')
 const navLinks = menu.querySelectorAll('a')
-menuBtn.addEventListener('click',()=>{
-   menu.classList.toggle('active')
-   setMenuIcon()
-   
-   
+menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('active')
+    setMenuIcon()
+
+
 })
-function setMenuIcon(){
-    if(menuBtn.classList.contains('ri-close-line')){
-        document.body.style.overflow="scroll"
-        menuBtn.classList.replace('ri-close-line','ri-menu-3-line')
-      }else{
-        document.body.style.overflow="hidden"
-        setTimeout(()=>{
-            menuBtn.classList.replace('ri-menu-3-line','ri-close-line')
-            
-           },500)
-      }
+function setMenuIcon() {
+    if (menuBtn.classList.contains('ri-close-line')) {
+        document.body.style.overflow = "scroll"
+        menuBtn.classList.replace('ri-close-line', 'ri-menu-3-line')
+    } else {
+        document.body.style.overflow = "hidden"
+        setTimeout(() => {
+            menuBtn.classList.replace('ri-menu-3-line', 'ri-close-line')
+
+        }, 500)
+    }
 }
 
-navLinks.forEach((link)=>{
-    link.addEventListener('click',()=>{
+let bWidth = window.innerWidth;
+console.log(bWidth)
+
+navLinks.forEach((link) => {
+    link.addEventListener('click', () => {
         menu.classList.toggle('active')
-       setMenuIcon()
+
+        if(bWidth<769){
+            setMenuIcon()
+        }
+
     })
 })
