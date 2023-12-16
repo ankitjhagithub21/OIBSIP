70d57f8d8b6af8e2e4cfd104c86de71c842d11eb
ankitjhagithub21 <ajha45075@gmail.com>
2023-12-16 09:09:58 +0530

    changes font

--- LEVEL 1/TASK 1/script.js
+++ LEVEL 1/TASK 1/script.js
@@ -1,35 +1,39 @@
-const menuBtn = document.getElementById('menuBtn')
-const menu = document.getElementById('menu')
-const navLinks = menu.querySelectorAll('a')
+const menuBtn = document.getElementById('menuBtn');
+const menu = document.getElementById('menu');
+const navLinks = menu.querySelectorAll('a');
+const bWidth = window.innerWidth;
+
 menuBtn.addEventListener('click', () => {
-    menu.classList.toggle('active')
-    setMenuIcon()
+    menu.classList.toggle('active');
+    setMenuIcon();
+});
 
-
-})
 function setMenuIcon() {
     if (menuBtn.classList.contains('ri-close-line')) {
-        document.body.style.overflow = "scroll"
-        menuBtn.classList.replace('ri-close-line', 'ri-menu-3-line')
+        document.body.style.overflow = "scroll";
+        menuBtn.classList.replace('ri-close-line', 'ri-menu-3-line');
     } else {
-        document.body.style.overflow = "hidden"
-        setTimeout(() => {
-            menuBtn.classList.replace('ri-menu-3-line', 'ri-close-line')
-
-        }, 500)
+        if (menuBtn.classList.contains('ri-menu-3-line')) {
+            document.body.style.overflow = "hidden";
+            menuBtn.classList.replace('ri-menu-3-line', 'ri-close-line');
+        }
     }
 }
 
-let bWidth = window.innerWidth;
-console.log(bWidth)
-
 navLinks.forEach((link) => {
     link.addEventListener('click', () => {
-        menu.classList.toggle('active')
+        menu.classList.toggle('active');
 
-        if(bWidth<769){
-            setMenuIcon()
+        if (bWidth < 768) {
+            setMenuIcon();
         }
+    });
+});
 
-    })
-})
+menuBtn.addEventListener('transitionend', () => {
+    if (menuBtn.classList.contains('ri-close-line')) {
+        document.body.style.overflow = "hidden";
+    } else {
+        document.body.style.overflow = "scroll";
+    }
+});
